e47070866a161e2dab588ef63259300b84c85251
ShivamBharadwaj31 <shivam.bhardwag_mca18@gla.ac.in>
2019-03-07 11:23:48 +0530

    code implement

--- app/src/main/java/MyLogIn/mylogin/MainActivity.java
+++ app/src/main/java/MyLogIn/mylogin/MainActivity.java
@@ -1,5 +1,6 @@
 package MyLogIn.mylogin;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences sharedPreferences=getSharedPreferences("MyLogIn.mylogin"MODE_PRIVATE);
+        SharedPreferences.Editor editor =sharedPreferences.edit();
+        editor.putStringS("username","shivam.bhardwag_mca18@gla.ac.in");
+        editor.putString("password","123456");
     }
 }
